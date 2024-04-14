978ed35830d77e09f0708ef9cb7a7dcf501a2856
Bogdan <159520057+ModestBody@users.noreply.github.com>
2024-04-14 12:38:32 +0300

    Adding main.cpp file.

---
+++ main.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+bool IsKPeriodic(const string& str, int K) 
+{
+    int len = str.length();
+    if (len % K != 0) 
+    {
+        return false;
+    }
+    for (int i = 0; i < len; ++i) 
+    {
+        if (str[i] != str[i % K]) 
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() 
+{
+    string input;
+    int K;
+    cout << "Enter the line: ";
+    cin >> input;
+
+    cout << "Enter K: ";
+    cin >> K;
+
+    if (IsKPeriodic(input, K)) 
+    {
+        cout << "The string is " << K << " - periodic." << endl;
+    }
+    else 
+    {
+        cout << "The string is not " << K << " - periodic." << endl;
+    }
+
+    return 0;
+}
